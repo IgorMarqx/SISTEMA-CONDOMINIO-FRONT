fe4cd31c9ac8fca44eb8f827a7dba216c50c9df2
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-21 20:56:47 -0300

    feat: implement dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,11 @@
+import Middleware from "@/app/components/middleware/middleware";
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    return(
+        <Middleware>
+            <div className={`w-screen h-screen`}>
+                <h1 className={`text-4xl text-center`}>Dashboard</h1>
+            </div>
+        </Middleware>
+    )
 }
