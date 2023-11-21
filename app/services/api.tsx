6c9203b4f8dacf24624e39c67da862ec5716aa29
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-21 20:55:45 -0300

    config: adjust the api requisitions

--- app/services/api.tsx
+++ app/services/api.tsx
@@ -1,13 +1,6 @@
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL
+
+import {post} from "./requisitions";
 export async function login(email: string, password: string) {
-    const response = await fetch("http://127.0.0.1:8000/api/auth/login", {
-        method: "POST",
-        body: JSON.stringify({
-            email,
-            password
-        }),
-        headers: {
-            "Content-Type": "application/json"
-        },
-    });
-    return await response.json();
+    return await post(`${BASE_URL}auth/login`, 'POST', {email, password})
 }
