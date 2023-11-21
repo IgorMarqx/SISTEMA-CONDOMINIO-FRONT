15eddcbe0803a7ba8a10f1a3dce86f96b8d84f63
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-21 20:56:21 -0300

    feat: implement layout

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar(){
+    return(
+        <div className={`flex justify-between p-4 bg-sky-500`}>
+            <div>
+                <h1>Logo</h1>
+            </div>
+
+            <div>
+                <h1>Logout</h1>
+            </div>
+        </div>
+    )
+}
