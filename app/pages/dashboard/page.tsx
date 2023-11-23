1c00b1cc396c29793a7b467d69c88fc6fec06dc7
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-23 20:25:02 -0300

    feat: implement middleware

---
+++ app/pages/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className={`w-screen h-screen`}>
+            <h1 className={`text-4xl text-center`}>Dashboard</h1>
+        </div>
+    )
+}
