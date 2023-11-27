56466cd12dfbdda47163c94edbd4e2a6ccf0cde4
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-27 20:55:02 -0300

    feat: implement layout style

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({children}: LayoutProps) {
         <>
             <Navbar/>
             <Navigationbar/>
-            {children}
+            <main>{children}</main>
         </>
     )
 }
