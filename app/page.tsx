cabb92d0f37a3f6f47de0b87a271229ab835b66b
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-27 20:55:55 -0300

    config: configure login page

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function Login() {
             }
 
             Cookies.set('token', data.token)
-            router.push("/dashboard");
+            router.push("pages/dashboard");
         })
     }
 
