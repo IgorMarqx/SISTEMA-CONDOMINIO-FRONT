487a47b00d72ff5aedcc93e9eb1bece0f695a3e9
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-29 22:17:23 -0300

    adjust: update on style while used tailwind

--- app/components/navigation/Navigationbar.tsx
+++ app/components/navigation/Navigationbar.tsx
@@ -1,7 +1,19 @@
-export default function Navigationbar(){
+import {ReactNode} from "react";
+import Links from "@/app/components/links/Links";
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+export default function Navigationbar({children}: LayoutProps){
     return(
-        <div className={`bg-green-500 h-screen`}>
-            asdasd
+        <div className={`flex h-screen shadow-2xl box-sizing:border-box`}>
+            <div className={`flex w-[20%] max-w-[20%] min-w-fit bg-[#3C3C3C] justify-center text-[#FFFFFF]`}>
+                <Links/>
+            </div>
+            <div className={`flex flex-1 bg-[#EEEEEE]`}>
+                {children}
+            </div>
         </div>
     )
 }
