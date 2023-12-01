091b368e9c97683d7931c14bb79a560b2224c7a6
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-30 22:48:48 -0300

    feat: implementando estado para abrir e fechar barra de navegaçao

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import {ReactNode} from "react";
+import {ReactNode, useState} from "react";
 import Navbar from "@/app/components/navbar/Navbar";
 import Navigationbar from "@/app/components/navigation/Navigationbar";
 
@@ -7,10 +7,12 @@ interface LayoutProps {
 }
 
 export default function Layout({children}: LayoutProps) {
+    const [isNavCollapsed, setIsNavCollapsed] = useState(false);
+
     return (
         <>
-            <Navbar/>
-            <Navigationbar>
+            <Navbar isNavCollapsed={isNavCollapsed} setIsNavCollapsed={setIsNavCollapsed} />
+            <Navigationbar isNavCollapsed={isNavCollapsed} >
                 {children}
             </Navigationbar>
         </>
