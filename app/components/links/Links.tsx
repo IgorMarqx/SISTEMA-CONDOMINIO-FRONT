f6dc2a618f97ca9808a268b50b9f6894abb46519
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-30 22:49:11 -0300

    feat: implementando animação para ficar mais suave ao fechar e abrir a barra

--- app/components/links/Links.tsx
+++ app/components/links/Links.tsx
@@ -1,15 +1,24 @@
 import {faGauge, faUsers} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import {motion} from 'framer-motion';
 
-export default function Links(){
+export default function Links({icon, label, to, isNavCollapsed}: any){
 return(
-        <div className={`w-[95%] min-w-fit`}>
-            <div className={`pt-1 pb-1 hover:bg-[#6e7073] hover:text-[#fafbfc] w-[100%]`}>
-                <a className={`text-lg pt-1 pb-1 rounded-sm text-start`} href={`/pages/dashboard`}><FontAwesomeIcon className={`mr-2 ml-2`} icon={faGauge} />Dashboard</a>
+    <div className={`w-[95%] min-w-fit`}>
+        <Link className={`text-lg text-start`} href={to}>
+            <div className={`pt-1 pb-1 hover:bg-[#6e7073] hover:text-[#fafbfc] w-[100%] rounded tex-base`}>
+                <FontAwesomeIcon className={`mr-2 ml-2`} icon={icon} />
+                <motion.span
+                    className={`ml-1`}
+                    initial={{opacity: 0, x: 0}}
+                    style={{display: isNavCollapsed ? 'none' : 'inline'}}
+                    animate={{opacity: isNavCollapsed ? 0 : 1, x: isNavCollapsed ? -50 : 0}}
+                    transition={{duration: .5}}>
+                    {label}
+                </motion.span>
             </div>
-            <div className={`pt-1 pb-1 hover:bg-[#6e7073] hover:text-[#fafbfc] w-[100%]`}>
-                <a className={`text-lg pt-1 pb-1 rounded-sm text-start`} href={`pages/dashboard`}><FontAwesomeIcon className={`mr-2 ml-2`} icon={faUsers} />Usuários</a>
-            </div>
-        </div>
+        </Link>
+    </div>
     )
 }
