76302d1d634e64b040b071658eda9ed4eb9fc98b
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-11-30 22:49:18 -0300

    feat: implementando animação para ficar mais suave ao fechar e abrir a barra

--- app/components/navigation/Navigationbar.tsx
+++ app/components/navigation/Navigationbar.tsx
@@ -1,16 +1,22 @@
 import {ReactNode} from "react";
 import Links from "@/app/components/links/Links";
+import {faGauge, faUsers} from "@fortawesome/free-solid-svg-icons";
+import { motion } from "framer-motion";
 
-interface LayoutProps {
-    children: ReactNode;
-}
-
-export default function Navigationbar({children}: LayoutProps){
+export default function Navigationbar({children, isNavCollapsed}: any){
     return(
         <div className={`flex h-screen shadow-2xl box-sizing:border-box`}>
-            <div className={`flex w-[20%] max-w-[20%] min-w-fit bg-[#3C3C3C] justify-center text-[#FFFFFF]`}>
-                <Links/>
-            </div>
+            <motion.div
+                className={`flex ${isNavCollapsed ? 'w-[74px]' : 'w-[250px]'} max-w-[250px] bg-[#3C3C3C] justify-center text-[#FFFFFF]`}   initial={{ width: isNavCollapsed ? 74 : 250 }}
+                 animate={{ width: isNavCollapsed ? 74 : 250 }}
+                 transition={{ duration: .5 }}>
+
+                <div className={`w-[100%] pl-4 pt-4`}>
+                    <Links icon={faGauge} label='Dashboard' to='/pages/dashboard' isNavCollapsed={isNavCollapsed} />
+                    <Links icon={faUsers} label='Usuários' to='/pages/users' isNavCollapsed={isNavCollapsed} />
+                </div>
+
+            </motion.div>
             <div className={`flex flex-1 bg-[#EEEEEE]`}>
                 {children}
             </div>
