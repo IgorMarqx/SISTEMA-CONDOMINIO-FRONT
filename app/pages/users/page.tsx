da13ab68238398c3611191a9e643444adfc70460
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-12-05 19:42:54 -0300

    implementando modal e componente de botao na pagina de usuarios

--- app/pages/users/page.tsx
+++ app/pages/users/page.tsx
@@ -3,13 +3,21 @@ import Auth from "@/app/HOC/auth";
 import Layout from "@/app/components/layout/Layout";
 import Table from "@/app/components/table/Table";
 import ButtonFunctions from "@/app/components/buttons/ButtonFunctions";
+import Modal from "@/app/components/modal/Modal";
+import {useState} from "react";
+import InputCreate from "@/app/components/inputs/InputCreate";
 
 function Users(){
+    const [isOpen, setIsOpen] = useState(false);
+
     return(
         <Layout>
             <div className={`flex flex-col flex-1`}>
                 <div className={`flex`}>
-                    <ButtonFunctions text={'Criar usuário'} color={'bg-[#3C3C3C]'} hoverColor={'bg-[#5c5b5b]'} />
+                    <ButtonFunctions onClick={() => setIsOpen(true)} text={'Criar usuário'} color={'bg-[#3C3C3C]'} hoverColor={'bg-[#5c5b5b]'} />
+                    <Modal modalTitle={'Criar usuário'} isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                        <InputCreate></InputCreate>
+                    </Modal>
                 </div>
                 <div className={`flex bg-white ml-4 mr-4 rounded-sm shadow h-96 flex-col`}>
                     <div className={`border-t-[3px] border-pink-600`}>
