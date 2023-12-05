68a94dcf09c92804bcd7c2ad18926b8a2c7981c6
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-12-04 22:45:58 -0300

    feat: criando componente de tabelas

---
+++ app/components/table/Table.tsx
@@ -0,0 +1,36 @@
+interface Props{
+    data: any
+}
+
+export default function Table(props: Props){
+    return(
+        <div>
+            <div className="relative overflow-x-auto">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            ID
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            NOME
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            E-MAIL
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            AÇÕES
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr className="bg-white border-b dark:bg-gray-800hover:bg-gray-50">
+                        {props.data}
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    )
+}
