4c3fb7967b6eb49d714497b5490fe9708ee65c34
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-12-05 19:41:52 -0300

    feat: implementando função de abrir modal no botao

--- app/components/buttons/ButtonFunctions.tsx
+++ app/components/buttons/ButtonFunctions.tsx
@@ -1,15 +1,17 @@
-interface Props{
+import { MouseEventHandler } from "react";
+
+interface Props {
     text: string;
     color: string;
     hoverColor: string;
-    onClick?: (e: any) => void;
-
+    onClick: MouseEventHandler<HTMLSpanElement>;
 }
-export default function ButtonFunctions(props: Props){
+
+export default function ButtonFunctions({ onClick, text, color, hoverColor }: Props){
     return(
         <div className={`flex`}>
-            <span className={`${props.color} hover:${props.hoverColor} pr-3 pl-3 pt-2 pb-2 rounded m-4 text-white cursor-pointer`}>
-                {props.text}
+            <span onClick={onClick} className={`${color} hover:${hoverColor} pr-3 pl-3 pt-2 pb-2 rounded m-4 text-white cursor-pointer`}>
+                {text}
             </span>
         </div>
     )
