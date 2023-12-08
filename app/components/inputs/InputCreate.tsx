ea26df9c466be56c9a7c2eb7c3e02d5f1de167f0
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-12-07 21:22:55 -0300

    feat: implementando mostragem de condominios

--- app/components/inputs/InputCreate.tsx
+++ app/components/inputs/InputCreate.tsx
@@ -1,12 +1,20 @@
-import InputLayout from "@/app/components/inputs/InputLayout";
+'use client'
 import Inputs from "@/app/components/inputs/Inputs";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import Select from "@/app/components/inputs/Select";
+import {getCondominiums} from "@/app/services/api";
 
-export default function InputCreate({ setInputData, inputData }: any) {
+export default function InputCreate({setInputData, inputData}: any) {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [condominio, setCondominio] = useState('');
+    const [condominium, setCondominium] = useState([]);
+
+    useEffect(() => {
+        getCondominiums().then((response) => {
+            setCondominium(response)
+        })
+    }, []);
 
     const handleNameChange = (e: any) => {
         const newName = e.target.value;
@@ -26,10 +34,17 @@ export default function InputCreate({ setInputData, inputData }: any) {
         setInputData({...inputData, password: newPassword})
     }
 
+    const handleCondominiumChange = (e: any) => {
+        const newCondominium = e.target.value
+        setCondominium([newCondominium])
+        setInputData({...inputData, condominium: newCondominium})
+    }
+
     return (
         <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-1/2 px-3">
                 <Inputs
+                    textFor="name"
                     label={'Nome'}
                     placeHolder={'Informe seu nome'}
                     required={'*'}
@@ -38,6 +53,7 @@ export default function InputCreate({ setInputData, inputData }: any) {
             </div>
             <div className="w-full md:w-1/2 px-3">
                 <Inputs
+                    textFor="email"
                     label={'Email'}
                     placeHolder={'Informe seu e-mail'}
                     required={'*'}
@@ -46,11 +62,27 @@ export default function InputCreate({ setInputData, inputData }: any) {
             </div>
             <div className="w-full md:w-1/1 px-3">
                 <Inputs
+                    textFor="password"
                     label={'Senha'}
                     placeHolder={'Informe uma senha'}
                     required={'(opcional)'}
                     value={password}
                     onChange={handlePasswordChange}/>
+            </div>
+            <div className="w-full md:w-1/1 px-3">
+                <Select
+                    textFor="condominio"
+                    label="Condominio"
+                    required="*"
+                    placeHolder="Select an option"
+                    value={condominium}
+                    onChange={handleCondominiumChange}
+                >
+                    <option disabled selected>Escolha um condominio</option>
+                    {condominium.map((condominium:any) => {
+                        return <option key={condominium.id} value={condominium.id}>{condominium.name}</option>
+                    })}
+                </Select>
             </div>
         </div>
     )
