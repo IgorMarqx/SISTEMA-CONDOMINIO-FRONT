00671b5ebbf2c288f878bf823f17f53bd97c311e
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-12-07 21:22:46 -0300

    feat: implementando requisição de getCondominium

--- app/services/api.tsx
+++ app/services/api.tsx
@@ -1,14 +1,19 @@
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL
 
-import {post} from "./requisitions";
+import {post, get} from "./requisitions";
+
 export async function login(email: string, password: string) {
-    return await post(`${BASE_URL}auth/login`, 'POST', {email, password})
+    return await post(`${BASE_URL}auth/login`, {email, password})
 }
 
 export async function verifyToken(token: string | undefined) {
-    return await post(`${BASE_URL}getToken`, 'POST', {token})
+    return await post(`${BASE_URL}getToken`, {token})
 }
 
-export async function register(name: string, email: string, password:string) {
-    return await post(`${BASE_URL}auth/register`, 'POST', {name, email, password})
+export async function register(name: string, email: string, password: string) {
+    return await post(`${BASE_URL}auth/register`, {name, email, password})
+}
+
+export async function getCondominiums() {
+    return await get(`${BASE_URL}condominium`)
 }
