344343512dbba7f5f11a8b4da8b102519126f2ab
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-12-13 19:47:49 -0300

    feat: implementando requisição para pegar usuarios

--- app/services/api.tsx
+++ app/services/api.tsx
@@ -1,3 +1,5 @@
+import {Requireable} from "react";
+
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL
 
 import {post, get} from "./requisitions";
@@ -10,10 +12,14 @@ export async function verifyToken(token: string | undefined) {
     return await post(`${BASE_URL}getToken`, {token})
 }
 
-export async function register(name: string, email: string, password: string) {
-    return await post(`${BASE_URL}auth/register`, {name, email, password})
+export async function register(name: string, email: string, password: string, condominium_id: Requireable<number>) {
+    return await post(`${BASE_URL}auth/register`, {name, email, password, condominium_id})
 }
 
 export async function getCondominiums() {
     return await get(`${BASE_URL}condominium`)
+}
+
+export async function getUsers(){
+    return await get(`${BASE_URL}users`)
 }
